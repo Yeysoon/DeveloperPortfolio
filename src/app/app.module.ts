1aed664cd55993f7ba7fc49a996c25325e8afbe3
Yeysoon <yeysonaldair13@gmail.com>
2024-12-24 16:39:53 -0600

    Created the proyect

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ExperienceComponent } from './Components/experience/experience.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    HomeComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
