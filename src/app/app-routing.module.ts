0ef83e10069d6a16617769cfb255835dfc67c4fb
Yeysoon <yeysonaldair13@gmail.com>
2024-12-24 18:09:56 -0600

    Definition of routes for pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component'; // Importa tus componentes
 import { AboutComponent } from './Components/about/about.component';
 import { ContactComponent } from './Components/contact/contact.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { ExperienceComponent } from './Components/experience/experience.component';
 
 const routes: Routes = [
+
+  //Redirigue las rutas a la pagina principal
+
+
+
   { path: 'home', component: HomeComponent }, // Define las rutas
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'projects', component: ProjectsComponent},
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home'}
+  { path: '**', redirectTo: '/home'},
 ];
 
 @NgModule({
