89cae4728ca9cba9b4a067e2466af43165ce35b0
Yeysoon <yeysonaldair13@gmail.com>
2024-12-25 21:54:40 -0600

    Created footer for portfolio

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: false,
-  
+
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent  {
+  public currentYear: number | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
 
 }
