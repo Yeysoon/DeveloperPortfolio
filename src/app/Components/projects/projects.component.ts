419cd557b65a167b32b70e8aa9570370b5e3fbba
Yeysoon <yeysonaldair13@gmail.com>
2025-01-12 08:10:36 -0600

    Images

--- src/app/Components/projects/projects.component.ts
+++ src/app/Components/projects/projects.component.ts
@@ -5,6 +5,8 @@ export interface Project {
   description: string;
   stats: string;
   image: string;
+  image2: string;
+  image3: string;
   technologies: string[];
   links: {
     code?: string;
@@ -28,6 +30,8 @@ export class ProjectsComponent {
       description: 'Library of SVG logos from the most popular brands.',
       stats: '+10k visits per month. +2K SVGs downloaded. Created from scratch with Next.js, React, and Tailwind CSS.',
       image: '/images/HospitalApp.png',
+      image2: '/images/HospitalApp2.png',
+      image3: '/images/HospitalApp3.png',
       technologies: ['Next.js', 'Tailwind CSS'],
       links: {
         code: 'https://github.com/username/svgl',
@@ -39,11 +43,38 @@ export class ProjectsComponent {
       description: 'Free platform with programming challenges.',
       stats: 'Over 1 million visits in a month. +50K challenges completed. Created from scratch with Next.js, React, and Tailwind CSS.',
       image: '/images/Abarroteria.png',
+      image2: '/images/Abarroteria2.png',
+      image3: '/images/Abarroteria3.png',
       technologies: ['Next.js', 'Tailwind CSS'],
       links: {
         preview: 'https://adventjs.example.com'
       }
     }
   ];
+
+  currentSlides: { [key: string]: number } = {};
+
+  constructor() {
+    this.projects.forEach(project => {
+      this.currentSlides[project.title] = 0; // Inicializa cada carrusel en la primera imagen
+    });
+  }
+
+  nextSlide(project: Project): void {
+    const images = [project.image, project.image2, project.image3];
+    this.currentSlides[project.title] = (this.currentSlides[project.title] + 1) % images.length;
+    this.updateSlider(project);
+  }
+
+  prevSlide(project: Project): void {
+    const images = [project.image, project.image2, project.image3];
+    this.currentSlides[project.title] = (this.currentSlides[project.title] - 1 + images.length) % images.length;
+    this.updateSlider(project);
+  }
+
+  updateSlider(project: Project): void {
+    const slider = document.querySelector(`.slider`) as HTMLElement;
+    slider.style.transform = `translateX(-${this.currentSlides[project.title] * 100}%)`;
+  }
 
 }
