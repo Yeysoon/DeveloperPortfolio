248ffbce95a34428c7bc82b103565a5fcfdc19b6
Yeysoon <yeysonaldair13@gmail.com>
2025-04-10 09:18:49 -0600

    Litle changes in experiens

--- src/app/Components/experience/experience.component.ts
+++ src/app/Components/experience/experience.component.ts
@@ -22,7 +22,7 @@ export class ExperienceComponent {
 
       role: 'Software QA Analyst',
       company: 'Arrenco Gasolineras Don Arturo · Jornada Completa',
-      date: 'Abril. 2025 - Presente · 8 meses | Guatemala',
+      date: 'Abril. 2025 - Presente · 2 meses | Mixco Guatemala',
       description: 'Especialista en la implementación de aplicaciones, sistemas y reporteria interna en el área de Recursos Humanos, con el objetivo de automatizar y facilitar procesos, administración de bases de datos, documentacion de aplicaciones, brinda soporte tecnico e identifica oportunidades de mejora en los sistemas de información, garantizando la eficiencia y seguridad de los datos corporativos.',
       knowMore: 'Microsoft SQL Server · STA Web · Visual Basic · Analisis de Datos · SQL Reporting Services',
       logo: '/logos/MprhWhite.png'
@@ -42,7 +42,7 @@ export class ExperienceComponent {
     {
       role: 'HR Metrics Assistant IT RRHH',
       company: 'MPRH · Jornada Completa',
-      date: 'Jul. 2024 - Dic. 2024 · 6 meses | Guatemala',
+      date: 'Jul. 2024 - Dic. 2024 · 6 meses | Palín Escuintla',
       description: 'Soporte en la implementación de aplicaciones, sistemas y reportería interna a Talento Humano, proporcionando asistencia técnica a todo el personal del departamento, contribuyendo a la automatización y eficiencia de los procesos, administración de bases de datos, y mejora continua de los sistemas de información.',
       knowMore: 'Microsoft SQL Server · STA Web · Visual Basic(VBA) · Analisis de Datos · Soporte en sitio',
       logo: '/logos/MprhWhite.png'
@@ -54,7 +54,7 @@ export class ExperienceComponent {
     {
       role: 'Production Assistant',
       company: 'Kerry · Jornada Completa',
-      date: 'Sept. 2022 - Jul. 2024 · 1 año 11 meses | Guatemala',
+      date: 'Sept. 2022 - Jul. 2024 · 1 año 11 meses | Amatitlan, Guatemala',
       description: 'Responsable del análisis de ordenes de producción, validando costos dentro de ERP SAP, cantidades proyectadas, carga y retornos de productos utilizados en recetas incluyendo cierre TECO a las mismas, confirmaciones y des confirmaciones de fases de tiempo de procesos, resolución de casos específicos en el proceso, inventario general del depto, seguimiento a la programación semanal de la producción entre otros.',
       knowMore: 'ERP SAP · Suite Office · Resolución de Problemas · Análisis de datos · Trabajo en equipo',
       logo: '/logos/KerryWhite.png'
@@ -64,7 +64,7 @@ export class ExperienceComponent {
   experiences3: Experience[] = [
     {
       role: 'Cost Entry Assistant, Accounting Assistant',
-      company: 'Grupo Alza · Jornada completa | Guatemala',
+      company: 'Grupo Alza · Jornada completa | Amatitlan, Guatemala',
       date: 'Nov. 2019 - Ago. 2022 · 2 años 10 meses',
       description: 'Responsable de realizar el análisis validación de costos de operaciones de carga, retorno y cierre de OP así como control general de las ordenes, transformaciones de estas para realizar despachos hacia las diferentes bodegas, ingreso financiero de importaciones por (DUCA) y el costo de la materia prima.',
       knowMore: 'INFOR LABEL · Suite Office · Habilidades Comunicativas · Empatía· Flexibilidad y Adaptabilidad',
