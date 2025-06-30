cda881697bf6b6e568773edc87aebe1e7968416a
Yeysoon <yeysonaldair13@gmail.com>
2025-06-30 09:43:54 -0600

    testing

--- src/app/Components/experience/experience.component.ts
+++ src/app/Components/experience/experience.component.ts
@@ -91,7 +91,7 @@ calcularTiempoTranscurrido(inicio: string | undefined): string {
   const mesInicio = mesesNombres[fechaInicio.getMonth()];
   const anioInicio = fechaInicio.getFullYear();
 
-  return `${mesInicio} ${anioInicio} – Presente · ${partes.join(' y ')}`;
+  return `${mesInicio} ${anioInicio} – Presente · ${partes.join('  ')}`;
 }
 
 }
